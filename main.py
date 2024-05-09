94ffefce84ec36f6b055dcb34725136ef6e02d3d
BECATRUE <becatrueyjh@gmail.com>
2024-05-09 14:59:25 +0900

    Load config info

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Server to communicate with a HighFinesse wavemeter."""
 
+import json
 import logging
 from contextlib import asynccontextmanager
 
@@ -8,12 +9,29 @@ from pydantic_settings import BaseSettings
 
 logger = logging.getLogger(__name__)
 
+configs = {}
+
 class Setting(BaseSettings):  # pylint: disable=too-few-public-methods
     """Setting to specify the target config file path."""
     config_path: str = "config.json"
 
 
 setting = Setting()
+
+
+def load_configs():
+    """Loads config information from the config file.
+
+    The file should have the following JSON structure:
+
+      {
+        "version": {version},
+        "dll_path": {dll_path},
+        "app_path": {app_path}
+      }
+    """
+    with open(setting.config_path, encoding="utf-8") as config_file:
+        configs.update(json.load(config_file))
 
 
 @asynccontextmanager
