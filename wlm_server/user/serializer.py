533fa4a2b4ac544833d775b010a755d510b09236
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-11 19:49:08 +0900

    Define serializer for user info
    See also: #4

---
+++ wlm_server/user/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import User
+
+class UserInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "password",
+            "team",
+        )
