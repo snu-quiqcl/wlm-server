d62194f482387a0d6c49105276ab071854ea215d
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-11 19:50:47 +0900

    Define serializer for team info
    See also: #4

---
+++ wlm_server/team/serializer.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Team
+
+class TeamInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = (
+            "id",
+            "name",
+        )
