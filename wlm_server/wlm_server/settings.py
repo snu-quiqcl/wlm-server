66666893915f9f9f81f70908c3c532a7e507685e
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-11 18:50:40 +0900

    Install Django REST framework
    See also: #4

--- wlm_server/wlm_server/settings.py
+++ wlm_server/wlm_server/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user.apps.UserConfig',
     'team.apps.TeamConfig',
     'channel.apps.ChannelConfig',
