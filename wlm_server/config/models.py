64cb78a3a67be645cccd661a4c48ddcdc21ff4f2
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-11 15:21:42 +0900

    Set blank option `True` of nullable fields
    See also: #3

--- wlm_server/config/models.py
+++ wlm_server/config/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from datetime import timedelta
 
 class Config(models.Model):
-    wlm_version = models.IntegerField(null=True, default=None)
-    wlm_dll_path = models.CharField(max_length=100, null=True, default=None)
-    wlm_app_path = models.CharField(max_length=100, null=True, default=None)
+    wlm_version = models.IntegerField(blank=True, null=True, default=None)
+    wlm_dll_path = models.CharField(max_length=100, blank=True, null=True, default=None)
+    wlm_app_path = models.CharField(max_length=100, blank=True, null=True, default=None)
     calib_ch = models.IntegerField()
     calib_freq = models.FloatField()
     lock_duration = models.DurationField(default=timedelta(minutes=5))
