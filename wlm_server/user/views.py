1437f2825b215ace9e11b2b4f51d159530d5eb37
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-11 19:16:57 +0900

    Implement sign-out feature
    See also: #4

--- wlm_server/user/views.py
+++ wlm_server/user/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.decorators import api_view
 
 @api_view(['POST'])
@@ -10,6 +10,16 @@ def sign_in(request):
     user = authenticate(username=username, password=password)
     if user is not None:
         login(request, user)
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=401)
+
+
+@api_view(['POST'])
+def sign_out(request):
+    user = request.user
+    if user.is_authenticated:
+        logout(request)
         return HttpResponse(status=200)
     else:
         return HttpResponse(status=401)
