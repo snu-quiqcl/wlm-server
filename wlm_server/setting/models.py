7d76b9018af0ce2f45bd489c25e7c1c6a7a0a05e
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-12 16:49:04 +0900

    Add `setting` model
    It consists of four fields.
    - `channel`
    - `exposure` and `period`: Set by user.
    - `created_at`: Auto-generated when a record is created.
    
    See also: #3

---
+++ wlm_server/setting/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from channel.models import Channel
+
+class Setting(models.Model):
+    channel = models.ForeignKey(
+        Channel,
+        on_delete=models.PROTECT,
+        related_name='setting_history'
+    )
+    exposure = models.FloatField()
+    period = models.DurationField()
+    created_at = models.DateTimeField(auto_now_add=True)
