07282f00ae04b47abbca4c1e889e16fbbb8ce3ca
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-12 15:49:37 +0900

    Fix Pylint warning
    See also: #11

--- wlm_server/user/admin.py
+++ wlm_server/user/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 @admin.register(User)
-class UserAdmin_(UserAdmin):
+class CustomUserAdmin(UserAdmin):
     fieldsets = [
         (
             None,
