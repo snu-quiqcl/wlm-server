cc965198ad63bfbcf0e9a7f0d48693476c473398
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-12 15:36:33 +0900

    Define custom user admin class
    See also: #11

--- wlm_server/user/admin.py
+++ wlm_server/user/admin.py
@@ -3,4 +3,31 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin_(UserAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['username', 'password', 'team'],
+            },
+        ),
+        (
+            'Advanced options',
+            {
+                'classes': ['collapse'],
+                'fields': ['is_superuser'],
+            },
+        ),
+        (
+            'Important dates',
+            {
+                'classes': ['collapse'],
+                'fields': ['last_login'],
+            },
+        ),
+    ]
+    list_display = ('username', 'id', 'team__name', 'is_superuser')
+    list_filter = ('team__name', 'is_superuser')
+    search_fields = ('username', 'team__name')
+    ordering = ('id',)
