fb3c7639d6bc51fe43c48639b762e3be82931948
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-12 17:26:46 +0900

    Add `event` model
    It consists of three fields.
    - `category`: Event type. There are six kinds of events.
    - `content`: Event content.
    - `occured_at`: Auto-generated when a record is created.
    
    See also: #3

---
+++ wlm_server/event/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Event(models.Model):
+    class EventType(models.TextChoices):
+        GENERAL = 'GN', ('general')
+        ERROR = 'ER', ('error')
+        WARNING = 'WN', ('warning')
+        LOCK = 'LK', ('lock')
+        OPERATION = 'OP', ('operation')
+        CONFIG = 'CF', ('config')
+
+    category = models.CharField(max_length=2, choices=EventType, default=EventType.GENERAL)
+    content = models.CharField(max_length=200)
+    occured_at = models.DateTimeField(auto_now_add=True)
