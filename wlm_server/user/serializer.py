66dfd060c1f6bacebd4421d4789343406a54b674
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-13 13:01:33 +0900

    Use team serializer in user serializer
    See also: #4

--- wlm_server/user/serializer.py
+++ wlm_server/user/serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
 from .models import User
+from team.serializer import TeamInfoSerializer
 
 class UserInfoSerializer(serializers.ModelSerializer):
+    team = TeamInfoSerializer()
+
     class Meta:
         model = User
         fields = (
