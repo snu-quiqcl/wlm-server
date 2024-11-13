4ae9a78a2ca35c660ac3caf2035660e39e95e146
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-13 13:32:23 +0900

    Remove parentheses to meet convention
    See also: #3

--- wlm_server/event/models.py
+++ wlm_server/event/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Event(models.Model):
     class EventType(models.TextChoices):
-        GENERAL = 'GN', ('general')
-        ERROR = 'ER', ('error')
-        WARNING = 'WN', ('warning')
-        LOCK = 'LK', ('lock')
-        OPERATION = 'OP', ('operation')
-        CONFIG = 'CF', ('config')
+        GENERAL = 'GN', 'general'
+        ERROR = 'ER', 'error'
+        WARNING = 'WN', 'warning'
+        LOCK = 'LK', 'lock'
+        OPERATION = 'OP', 'operation'
+        CONFIG = 'CF', 'config'
 
     category = models.CharField(max_length=2, choices=EventType, default=EventType.GENERAL)
     content = models.CharField(max_length=200)
