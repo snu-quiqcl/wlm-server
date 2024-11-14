b83741efbd33ed7f1800eca811d775dd4dd7a3ee
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-15 03:36:30 +0900

    Define message info
    See also: #5

--- wlm_server/task/message.py
+++ wlm_server/task/message.py
@@ -2,6 +2,7 @@
 
 import dataclasses
 import enum
+from typing import Any
 
 class ActionType(enum.Enum):
     """Action type."""
@@ -12,7 +13,22 @@ class ActionType(enum.Enum):
 
 @dataclasses.dataclass
 class MessageInfo:
-    """Message info."""
+    """Message info.
+    
+    Fields:
+        action: Action type.
+        channel: Target channel. If None, it targets WLM.
+        data: Additional arguments for action. The arguments required for each action type are as follows.
+          OPERATE:
+            on (bool): If True, start measurement. Otherwise, stop measurement.
+          EXPOSURE:
+            exposure (datetime.timedelta): New exposure time.
+          PERIOD:
+            period (datetime.timedelta): New period.
+    """
+    action: ActionType
+    channel: int | None
+    data: dict[str, Any]
 
 
 class messageQueue:
