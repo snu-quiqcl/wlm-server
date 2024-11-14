3d1c59d31a0526c00db9c4d8f8fb2db648bbae7c
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-14 18:19:37 +0900

    Define action type
    See also: #5

--- wlm_server/task/message.py
+++ wlm_server/task/message.py
@@ -1,1 +1,19 @@
 """Module for message queue from request handler to task handler."""
+
+import dataclasses
+import enum
+
+class ActionType(enum.Enum):
+    """Action type."""
+    OPERATE = "operate"
+    EXPOSURE = "exposure"
+    PERIOD = "period"
+
+
+@dataclasses.dataclass
+class MessageInfo:
+    """Message info."""
+
+
+class messageQueue:
+    """Thread-safe message queue from request handler to task handler."""
