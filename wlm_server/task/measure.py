738975b7ed5b81bd12422001194788ec0957bfa1
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-15 04:10:19 +0900

    Define measurement info
    See also: #5

--- wlm_server/task/measure.py
+++ wlm_server/task/measure.py
@@ -1,1 +1,15 @@
 """Module for prioritized measurement queue."""
+
+import dataclasses
+from datetime import timedelta
+
+@dataclasses.dataclass
+class MeasureInfo:
+    """Measurement info.
+    
+    Fields:
+        channel: Target channel.
+        deadline: Measurement deadline.
+    """
+    channel: int
+    deadline: timedelta
