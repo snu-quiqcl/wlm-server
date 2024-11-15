df53a2d79bdbefef455cc1491e9cf4006125bd2e
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-15 11:21:06 +0900

    Define measurement queue class
    See also: #5

--- wlm_server/task/measure.py
+++ wlm_server/task/measure.py
@@ -1,6 +1,7 @@
 """Module for prioritized measurement queue."""
 
 import dataclasses
+import heapq
 from datetime import timedelta
 
 @dataclasses.dataclass
@@ -13,3 +14,32 @@ class MeasureInfo:
     """
     channel: int
     deadline: timedelta
+
+
+class MeasureQueue:
+    """Prioritized measurement queue based on deadlines."""
+
+    def __init__(self):
+        self._queue = []
+
+    def push(self, measure: MeasureInfo):
+        """Pushes the given measurement to the measurement queue.
+        
+        Args:
+            measure: Measurement info.
+        """
+        priority = measure.deadline
+        heapq.heappush(self._queue, (priority, measure))
+
+    def pop(self) -> MeasureInfo | None:
+        """Pops the measurement with the earlist deadline from the measurement queue.
+
+        Returns:
+            The measurement with the earlist deadline. If there are no scheduled measurements, it
+              returns None.
+        """
+        try:
+            item = heapq.heappop(self._queue)
+            return item[1]
+        except IndexError:
+            return None
