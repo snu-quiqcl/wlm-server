e14a5d13356881c5f77e0203605f0923eaec33d7
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-15 18:09:00 +0900

    Define task handler class
    See also: #5

--- wlm_server/task/handler.py
+++ wlm_server/task/handler.py
@@ -1,1 +1,14 @@
 """Module for task handler with WLM."""
+
+import threading
+
+class TaskHandler(threading.Thread):
+    """Task handler for controlling and monitoring WLM.
+    
+    Workflow:
+        1. Check if there are remaining messages in message queue. If so, perform all tasks.
+        2. Get the most prioritized measurement from measurement queue.
+        3. Perform the measurement and notify the result to request handler.
+        4. Put the next measurement to measurement queue.
+        5. Repeat steps 1 through 4.
+    """
