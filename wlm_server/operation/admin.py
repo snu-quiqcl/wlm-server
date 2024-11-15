1b9ce2b521cdef131f5587dd695df0b0e81b6def
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-16 04:11:50 +0900

    Add `ModelAdmin` for `operation` model
    See also: #12

--- wlm_server/operation/admin.py
+++ wlm_server/operation/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Operation
 
-admin.site.register(Operation)
+@admin.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['user', 'channel', 'on', 'occured_at'],
+            },
+        ),
+    ]
+    list_display = ['id', 'user', 'channel', 'on', 'occured_at']
+    list_filter = ['user', 'channel', 'on']
+    search_fields = ['user']
+    ordering = ['occured_at']
+    readonly_fields = ['occured_at']
