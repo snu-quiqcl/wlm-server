4aa46c4942813ae76bab953627e9250742438d01
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-16 04:13:55 +0900

    Add `ModelAdmin` for `calibration` model
    See also: #12

--- wlm_server/calibration/admin.py
+++ wlm_server/calibration/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Calibration
 
-admin.site.register(Calibration)
+@admin.register(Calibration)
+class CalibrationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['user', 'occured_at'],
+            },
+        ),
+    ]
+    list_display = ['id', 'user', 'occured_at']
+    list_filter = ['user']
+    search_fields = ['user']
+    ordering = ['occured_at']
+    readonly_fields = ['occured_at']
