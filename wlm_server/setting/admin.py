524a4f9e9a5271b389f4d937b39b733c40748efc
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-16 03:34:50 +0900

    Add `ModelAdmin` for `setting` model
    See also: #12

--- wlm_server/setting/admin.py
+++ wlm_server/setting/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Setting
 
-admin.site.register(Setting)
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': ['channel', 'exposure', 'period', 'created_at'],
+            },
+        ),
+    ]
+    list_display = ['id', 'channel', 'exposure', 'period', 'created_at']
+    list_filter = []
+    search_fields = ['exposure', 'period']
+    ordering = ['created_at']
+    readonly_fields = ['created_at']
