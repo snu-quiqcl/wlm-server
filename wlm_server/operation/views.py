9c3d698446fa368a2024f9ad290135aad26d331e
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-21 18:13:26 +0900

    Call `notify()` when operation status is changed
    See also: #17

--- wlm_server/operation/views.py
+++ wlm_server/operation/views.py
@@ -1,7 +1,11 @@
+import json
+
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from rest_framework.decorators import api_view
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 from operation.models import Operation
 from channel.models import Channel
@@ -44,6 +48,11 @@ def handle_info(request, ch: int):
         if not is_wlm_running:
             task_handler = TaskHandler()
             task_handler.start()
+            notif = {'on': True}
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                f'channel_{ch}', {'type': 'notify', 'message': json.dumps(notif)}
+            )
         if not is_channel_running:
             message = MessageInfo(ActionType.OPERATE, ch, {'on': True})
             message_queue.push(message)
@@ -57,4 +66,9 @@ def handle_info(request, ch: int):
         if not is_wlm_running:
             message = MessageInfo(ActionType.CLOSE, None, None)
             message_queue.push(message)
+            notif = {'on': False}
+            channel_layer = get_channel_layer()
+            async_to_sync(channel_layer.group_send)(
+                f'channel_{ch}', {'type': 'notify', 'message': json.dumps(notif)}
+            )
     return HttpResponse(status=200)
