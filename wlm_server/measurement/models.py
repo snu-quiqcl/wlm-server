daba6f8e88d9bc0910684aeee386892b057bc405
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-22 14:27:47 +0900

    Add `error` field to `measurement` model
    The measurement might be underexposed or overexposed, hence I added the
    `error` field to handle it.
    
    See also: #18

--- wlm_server/measurement/models.py
+++ wlm_server/measurement/models.py
@@ -3,17 +3,23 @@ from django.db import models
 from setting.models import Setting
 
 class Measurement(models.Model):
+    class ErrorType(models.TextChoices):
+        UNDER = 'under', 'underexposed'
+        OVER = 'over', 'overexposed'
+
     setting = models.ForeignKey(
         Setting,
         on_delete=models.PROTECT,
         related_name='measure_history',
     )
-    frequency = models.FloatField()
+    frequency = models.FloatField(blank=True, null=True, default=None)
+    error = models.CharField(max_length=10, choices=ErrorType, null=True, default=None)
     measured_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return (
             f'Setting: {self.setting}, '
             f'Frequency: {self.frequency}, '
+            f'Error: {self.error}, '
             f'Measured at: {self.measured_at}'
         )
