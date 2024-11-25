868787a9ae40ffe7e305af934e144a463faf8c97
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-26 01:51:14 +0900

    Remove redundant docstrings
    See also: #34

--- wlm_server/utils/util.py
+++ wlm_server/utils/util.py
@@ -7,20 +7,13 @@ def is_channel_running(channel: int) -> bool:
     
     Args:
         channel: Target channel.
-
-    Returns:
-        Whether the channel is currently running.
     """
     operations = settings.CHANNEL_CACHE.get_operations(channel)
     return any(op.on for op in operations.values())
 
 
 def is_wlm_running() -> bool:
-    """Returns whether the WLM is currently running.
-    
-    Returns:
-        Whether the WLM is currently running.
-    """
+    """Returns whether the WLM is currently running."""
     for channel in Channel.objects.all():
         if is_channel_running(channel.channel):
             return True
