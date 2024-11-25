e6d253608071a26a4636260e6cb9ed8036628db6
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-25 17:23:28 +0900

    Register callback function for saving operation
    See also: #34

--- wlm_server/operation/apps.py
+++ wlm_server/operation/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class OperationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'operation'
+
+    def ready(self):
+        from . import signals  # pylint: disable=import-outside-toplevel, unused-import
