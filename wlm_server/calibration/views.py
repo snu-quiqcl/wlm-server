44c65fa3a2f320fba8ca05fbbcfe9339f96a5f57
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-26 15:21:19 +0900

    Verify if user has permission to access channel
    See also: #19

--- wlm_server/calibration/views.py
+++ wlm_server/calibration/views.py
@@ -2,10 +2,16 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view
 
+from config.models import Config
 from utils import util
 
 @login_required
 @api_view(['POST'])
 def calibrate(request):
+    config = Config.objects.first()
+    user = request.user
+    channel, error_code = util.verify_channel_access(user, config.calib_ch)
+    if channel is None:
+        return HttpResponse(status=error_code)
     if util.is_wlm_running:
         return HttpResponse(409)
