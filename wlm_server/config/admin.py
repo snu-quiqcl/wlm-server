0d949963e1fbbd44c590e6fcf5e237b233dbad59
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-26 16:14:04 +0900

    Update admin page for `config` model
    See also: #19

--- wlm_server/config/admin.py
+++ wlm_server/config/admin.py
@@ -9,12 +9,12 @@ class ConfigAdmin(admin.ModelAdmin):
             None,
             {
                 'fields': ['wlm_version', 'wlm_dll_path', 'wlm_app_path',
-                           'calib_ch', 'calib_freq', 'lock_duration'],
+                           'calib_ch', 'calib_exposure', 'calib_freq', 'lock_duration'],
             },
         ),
     ]
     list_display = ['id', 'wlm_version', 'wlm_dll_path', 'wlm_app_path',
-                    'calib_ch', 'calib_freq', 'lock_duration']
+                    'calib_ch', 'calib_exposure', 'calib_freq', 'lock_duration']
     list_filter = []
     search_fields = []
     ordering = []
