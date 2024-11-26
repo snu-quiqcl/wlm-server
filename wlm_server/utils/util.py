79f68d0a95647d559d4cc72c66dd667d46afa67f
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-26 14:39:28 +0900

    Implement function to verify channel access
    See also: #19

--- wlm_server/utils/util.py
+++ wlm_server/utils/util.py
@@ -1,6 +1,27 @@
 from django.conf import settings
 
+from user.models import User
 from channel.models import Channel
+
+def verify_channel_access(user: User, ch: int) -> tuple[Channel | None, int | None]:
+    """Verifies if the user has permission to access the channel.
+    
+    Args:
+        user: User requesting access.
+        ch: Target channel.
+
+    Returns:
+        (channel, error_code): channel is the corresponding channel object. If an error occurs, it
+          is set to None. error_code is the HTTP status code for the occured error. If there is no
+          error, it is set to None.
+    """
+    try:
+        channel = Channel.objects.get(channel=ch)
+    except Channel.DoesNotExist:
+        return None, 404
+    if not channel.teams.contains(user.team):
+        return None, 403
+    return channel, None
 
 def is_channel_running(channel: int) -> bool:
     """Returns whether the given channel is currently running.
