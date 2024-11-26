54a1e130d283bebd7e3ef5e959026ba0a1f5a497
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-26 16:00:15 +0900

    Perform calibration in request handler
    See also: #19

--- wlm_server/calibration/views.py
+++ wlm_server/calibration/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from rest_framework.decorators import api_view
 
 from config.models import Config
+from task.message import ActionType, MessageInfo, MessageQueue
+from task.handler import TaskHandler
 from utils import util
 
 @login_required
@@ -15,3 +18,12 @@ def calibrate(request):
         return HttpResponse(status=error_code)
     if util.is_wlm_running:
         return HttpResponse(409)
+    message_queue: MessageQueue = settings.MESSAGE_QUEUE
+    task_handler = TaskHandler()
+    task_handler.start()
+    message = MessageInfo(ActionType.CALIB, config.calib_ch,
+                          {'exposure': config.calib_exposure, 'freq': config.calib_freq})
+    message_queue.push(message)
+    message = MessageInfo(ActionType.CLOSE, None, None)
+    message_queue.push(message)
+    return HttpResponse(status=200)
