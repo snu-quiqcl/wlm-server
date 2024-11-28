173154033edab486b8661e964a7c3ff6e2d24d25
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-28 11:39:41 +0900

    Add `operation` field to channel info serializer
    See also: #37

--- wlm_server/channel/serializer.py
+++ wlm_server/channel/serializer.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
 
 from .models import Channel
+from operation.models import Operation
 
 class ChannelInfoSerializer(serializers.ModelSerializer):
+    operation = serializers.SerializerMethodField()
+
     class Meta:
         model = Channel
         fields = (
             'channel',
             'name',
+            'operation',
         )
+
+    def get_operation(self, obj: Channel):
+        user = self.context['user']
+        try:
+            operation = Operation.objects.filter(user=user, channel=obj).latest('occurred_at')
+        except Operation.DoesNotExist:
+            return False
+        return operation.on
