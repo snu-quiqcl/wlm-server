89d6412ed9d1e873708ff3052909970da724d9d6
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-28 14:21:31 +0900

    Rename `operation` to `in_use`
    See also: #37

--- wlm_server/channel/serializer.py
+++ wlm_server/channel/serializer.py
@@ -4,17 +4,17 @@ from operation.models import Operation
 from .models import Channel
 
 class ChannelInfoSerializer(serializers.ModelSerializer):
-    operation = serializers.SerializerMethodField()
+    in_use = serializers.SerializerMethodField()
 
     class Meta:
         model = Channel
         fields = (
             'channel',
             'name',
-            'operation',
+            'in_use',
         )
 
-    def get_operation(self, obj: Channel):
+    def get_in_use(self, obj: Channel):
         user = self.context['user']
         try:
             operation = Operation.objects.filter(user=user, channel=obj).latest('occurred_at')
