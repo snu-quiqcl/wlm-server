82609020d8c9fc3d3b0f798ec5895f391ecaf0a3
Jaehun You <jaehun0121@snu.ac.kr>
2024-11-29 20:19:49 +0900

    Convert to Camel case
    See also: #37

--- wlm_server/channel/views.py
+++ wlm_server/channel/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from camel_converter import dict_to_camel
 
 from channel.models import Channel
 from channel.serializer import ChannelInfoSerializer
@@ -11,4 +12,4 @@ def handle_info(request):
     user = request.user
     channels = Channel.objects.filter(teams=user.team)
     data = ChannelInfoSerializer(channels, many=True, context={'user': user}).data
-    return Response(data)
+    return Response(dict_to_camel(data))
