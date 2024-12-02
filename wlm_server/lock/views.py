770c51713f4072341550a02479db0bc94a61eb79
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-02 20:23:05 +0900

    Implement to release lock
    See also: #20

--- wlm_server/lock/views.py
+++ wlm_server/lock/views.py
@@ -3,6 +3,7 @@ import json
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.utils import timezone
 from rest_framework.decorators import api_view
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
@@ -20,6 +21,24 @@ def try_lock(request, ch: int):
     lock = Lock(user=user, channel=channel)
     lock.save()
     notif = {'locked': True, 'username': user.username}
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f'channel_{ch}_lock', {'type': 'notify', 'message': json.dumps(notif)}
+    )
+    return HttpResponse(status=200)
+
+
+@login_required
+@api_view(['PUT'])
+def release_lock(request, ch: int):
+    user = request.user
+    channel, error_code = util.verify_channel_access(user, ch)
+    if channel is None:
+        return HttpResponse(status=error_code)
+    lock = settings.CHANNEL_CACHE.get_lock(ch)
+    lock.expires_at = timezone.now()
+    lock.save()
+    notif = {'locked': False, 'username': None}
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
         f'channel_{ch}_lock', {'type': 'notify', 'message': json.dumps(notif)}
