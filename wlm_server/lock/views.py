71768b826251bb03afc29ba8369296efc1c7e68c
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-02 19:56:35 +0900

    Implement to try lock
    See also: #20

---
+++ wlm_server/lock/views.py
@@ -0,0 +1,30 @@
+import json
+
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from rest_framework.decorators import api_view
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+from lock.models import Lock
+from utils import util
+
+@login_required
+@api_view(['POST'])
+def try_lock(request, ch: int):
+    user = request.user
+    channel, error_code = util.verify_channel_access(user, ch)
+    if channel is None:
+        return HttpResponse(status=error_code)
+    latest_lock = settings.CHANNEL_CACHE.get_lock(ch)
+    if latest_lock is not None:
+        return HttpResponse(status=409)
+    lock = Lock(user=user, channel=channel)
+    lock.save()
+    notif = {'locked': True, 'username': user.username}
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f'channel_{ch}_lock', {'type': 'notify', 'message': json.dumps(notif)}
+    )
+    return HttpResponse(status=200)
