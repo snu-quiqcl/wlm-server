466350108b0d64056df704c74440603bce5a71cb
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-05 14:38:05 +0900

    Add url pattern for offering single channel info
    See also: #42

--- wlm_server/channel/urls.py
+++ wlm_server/channel/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.handle_info, name='handle channels info'),
+    path('<int:ch>/', views.handle_single_info, name='handle single channel info'),
 ]
