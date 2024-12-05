9cdb6a6560851322bad888250e739e850d3aae43
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-05 14:37:29 +0900

    Offer single channel info
    See also: #42

--- wlm_server/channel/views.py
+++ wlm_server/channel/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -5,6 +6,7 @@ from camel_converter import dict_to_camel
 
 from channel.models import Channel
 from channel.serializers import ChannelInfoSerializer
+from utils import util
 
 @login_required
 @api_view(['GET'])
@@ -13,3 +15,14 @@ def handle_info(request):
     channels = Channel.objects.filter(teams=user.team)
     data = ChannelInfoSerializer(channels, many=True, context={'username': user.username}).data
     return Response([dict_to_camel(info) for info in data])
+
+
+@login_required
+@api_view(['GET'])
+def handle_single_info(request, ch: int):
+    user = request.user
+    channel, error_code = util.verify_channel_access(user, ch, False)
+    if channel is None:
+        return HttpResponse(status=error_code)
+    data = ChannelInfoSerializer(channel, context={'username': user.username}).data
+    return Response(dict_to_camel(data))
