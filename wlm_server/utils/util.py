06df1b6ca86551f260df670f08733a0bac50ba52
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-10 17:19:30 +0900

    Implement decorator for general lock
    See also: #40

--- wlm_server/utils/util.py
+++ wlm_server/utils/util.py
@@ -1,4 +1,7 @@
+import functools
 import json
+import threading
+from typing import Any, Callable
 
 from django.conf import settings
 from channels.layers import get_channel_layer
@@ -72,3 +75,13 @@ def record_event(category: Event.EventType, content: str):
         'event',
         {'type': 'notify', 'message': json.dumps(dict_to_camel(notif))}
     )
+
+
+def _synchronized(lock: threading.Lock) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            with lock:
+                return func(*args, **kwargs)
+        return wrapper
+    return decorator
