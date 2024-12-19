9802bb7541157fe8998581c4fbf7eb53fc07851e
Jaehun You <jaehun0121@snu.ac.kr>
2024-12-19 16:00:36 +0900

    Add `CSRF_TRUSTED_ORIGINS` to production settings
    I'm not sure, however, for HTTPS it looks necessary.
    
    See also: #47

--- wlm_server/wlm_server/settings/production.py
+++ wlm_server/wlm_server/settings/production.py
@@ -5,3 +5,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', config('SERVER_IP')]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost', f'https://{config('SERVER_IP')}']
