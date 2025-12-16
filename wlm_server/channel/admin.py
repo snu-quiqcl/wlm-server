77404829146dac75469e658d3c26935d420890c0
Jaehun You <jaehun0121@snu.ac.kr>
2025-12-16 13:20:42 +0900

    Update `Channel` admin page
    See also: #70

--- wlm_server/channel/admin.py
+++ wlm_server/channel/admin.py
@@ -12,7 +12,7 @@ class ChannelAdmin(admin.ModelAdmin):
             },
         ),
     ]
-    list_display = ['channel', 'name', 'dac_device', 'dac_channel']
-    list_filter = ['teams', 'dac_device']
-    search_fields = ['name', 'dac_device', 'dac_channel']
+    list_display = ['channel', 'name', 'dac_device__name', 'dac_channel']
+    list_filter = ['teams', 'dac_device__name']
+    search_fields = ['name', 'dac_device__name', 'dac_channel']
     ordering = ['channel']
