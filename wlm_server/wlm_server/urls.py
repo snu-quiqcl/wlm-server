0b37ed0f81790e6bc46a58d746138ebaa4244eba
Jaehun You <jaehun0121@snu.ac.kr>
2025-12-22 14:51:01 +0900

    Add url for PID toggle
    See also: #80

--- wlm_server/wlm_server/urls.py
+++ wlm_server/wlm_server/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('api/operation/', include('operation.urls')),
     path('api/calibration/', include('calibration.urls')),
     path('api/lock/', include('lock.urls')),
+    path('api/pid_operation/', include('pid_operation.urls')),
     path('admin/', admin.site.urls),
 ]
