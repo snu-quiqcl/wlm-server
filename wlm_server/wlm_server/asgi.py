9ddd73a0a7e20de11e0727bb4799e60b2f4261f9
Jaehun You <jaehun0121@snu.ac.kr>
2025-12-22 15:43:01 +0900

    Add url to asgi
    See also: #80

--- wlm_server/wlm_server/asgi.py
+++ wlm_server/wlm_server/asgi.py
@@ -27,6 +27,7 @@ from setting.consumers import SettingConsumer
 from measurement.consumers import MeasurementConsumer
 from lock.consumers import LockConsumer
 from pid_setting.consumers import DacControlConsumer, DacOutputConsumer
+from pid_operation.consumers import PidOperationConsumer
 from event.consumers import EventConsumer
 
 settings.CHANNEL_CACHE = ChannelCache()
@@ -43,6 +44,7 @@ application = ProtocolTypeRouter(
                     path('ws/lock/<int:ch>/', LockConsumer.as_asgi()),
                     path('ws/pid_setting/dac_control/<int:ch>/', DacControlConsumer.as_asgi()),
                     path('ws/pid_setting/dac_output/<int:ch>/', DacOutputConsumer.as_asgi()),
+                    path('ws/pid_operation/<int:ch>/', PidOperationConsumer.as_asgi()),
                     path('ws/event/', EventConsumer.as_asgi()),
                 ])
             )
