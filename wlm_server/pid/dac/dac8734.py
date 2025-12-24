16c4481b79ecc3f79b944ea446a7ad212df028c3
Jaehun You <jaehun0121@snu.ac.kr>
2025-12-24 17:21:06 +0900

    Use lazy formatting in logging
    See also: #60

--- wlm_server/pid/dac/dac8734.py
+++ wlm_server/pid/dac/dac8734.py
@@ -31,19 +31,19 @@ class DAC8734(BaseDAC):
     def open(self):
         """Overridden."""
         if self._is_open:
-            logger.warning(f'DAC8734 on {self._port} is already open')
+            logger.warning('DAC8734 on %s is already open', self._port)
             return
         try:
             self._fpga = ArtyS7(self._port)
         except serial.SerialException as e:
-            logger.error(f'Failed to open DAC8734: {e}')
+            logger.error('Failed to open DAC8734: %s', e)
         else:
             self._is_open = True
 
     def close(self):
         """Overridden."""
         if not self._is_open:
-            logger.warning(f'DAC8734 on {self._port} is not open')
+            logger.warning('DAC8734 on %s is not open', self._port)
             return
         self._fpga.close()
         self._fpga = None
@@ -52,10 +52,10 @@ class DAC8734(BaseDAC):
     def set_voltage(self, channel: int, voltage: float):
         """Overridden."""
         if not 0 <= channel < self.NUM_CHANNELS:
-            logger.warning(f'Invalid channel: {channel}')
+            logger.warning('Invalid channel: %d', channel)
             return
         if not self.MIN_V <= voltage <= self.MAX_V:
-            logger.warning(f'Voltage out of range: {voltage}')
+            logger.warning('Voltage out of range: %.4f V', voltage)
             return
         code = self._volts_to_code(voltage)
         dac_number = channel // 4
