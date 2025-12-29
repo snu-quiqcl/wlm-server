484431e3fb8175bfd68a5169c597f54f7267acf3
Jaehun You <jaehun0121@snu.ac.kr>
2025-12-29 12:57:56 +0900

    Make duplicate codes functions
    See also: #86

--- wlm_server/pid_setting/consumers.py
+++ wlm_server/pid_setting/consumers.py
@@ -5,7 +5,6 @@ from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from camel_converter import dict_to_camel
 
-from channel.models import Channel
 from pid.dac_control import DacControlInfo
 from utils import util
 
@@ -30,11 +29,6 @@ class DacControlConsumer(AsyncWebsocketConsumer):
             return
         await self.accept()
         self.channel = channel
-
-    @database_sync_to_async
-    def _check_dac_info(self, channel: Channel):
-        """Checks if channel has DAC device and channel information."""
-        return channel.dac_device is not None and channel.dac_channel is not None
 
     async def receive(self, text_data=None, bytes_data=None):
         """Receives the DAC control commands and sends them to the DAC control queue.
@@ -72,23 +66,11 @@ class DacOutputConsumer(AsyncWebsocketConsumer):
         if channel is None:
             await self.close(code=error_code)
             return
-<<<<<<< HEAD
-=======
-        has_dac_info = await self._check_dac_info(channel)
-        if not has_dac_info:
-            await self.close(code=400)
-            return
->>>>>>> 0dba8a1 (Use `database_sync_to_async` decorator)
         self.group_name = f'channel_{ch}_dac_output'
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
         voltage = settings.CHANNEL_CACHE.get_dac_voltage(ch)
         await self.send(text_data=json.dumps({'voltage': voltage}))
-
-    @database_sync_to_async
-    def _check_dac_info(self, channel: Channel):
-        """Checks if channel has DAC device and channel information."""
-        return channel.dac_device is not None and channel.dac_channel is not None
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
@@ -125,13 +107,6 @@ class PidSettingConsumer(AsyncWebsocketConsumer):
         if channel is None:
             await self.close(code=error_code)
             return
-<<<<<<< HEAD
-=======
-        has_dac_info = await self._check_dac_info(channel)
-        if not has_dac_info:
-            await self.close(code=400)
-            return
->>>>>>> 0dba8a1 (Use `database_sync_to_async` decorator)
         self.group_name = f'channel_{ch}_pid_setting'
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
@@ -142,11 +117,6 @@ class PidSettingConsumer(AsyncWebsocketConsumer):
             'ki': pid_setting.ki,
             'kd': pid_setting.kd
         })))
-
-    @database_sync_to_async
-    def _check_dac_info(self, channel: Channel):
-        """Checks if channel has DAC device and channel information."""
-        return channel.dac_device is not None and channel.dac_channel is not None
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
